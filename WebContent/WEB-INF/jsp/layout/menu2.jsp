b849e10a43f7ec2c85c9e75aa40992d4427f867a
admin <admin@YD02-22>
2021-05-27 17:55:45 +0900

    0527-1

--- WebContent/WEB-INF/jsp/layout/menu2.jsp
+++ WebContent/WEB-INF/jsp/layout/menu2.jsp
@@ -26,6 +26,7 @@
                           <c:when test="${!empty id }">
                           <form class="d-flex" action="">
                               <button class="btn btn-outline-dark" type="submit">
+                         	
                               
                                   <i class="bi-cart-fill me-1"></i>Cart
                                   <span class="badge bg-dark text-white ms-1 rounded-pill">${cartCnt }</span>
--- src/com/yedam/product/cart/CartList.java
+++ src/com/yedam/product/cart/CartList.java
@@ -1,15 +1,24 @@
 package com.yedam.product.cart;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.yedam.common.DbCommand;
+import com.yedam.product.ProductVO;
+import com.yedam.product.service.ProductService;
+import com.yedam.product.serviceImpl.ProductServiceImpl;
 
 public class CartList implements DbCommand{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
 	
+		ProductService service = new ProductServiceImpl();
+		List<ProductVO> list = service.productSelectList();
+		
+		//request.setAttribute();
 		return "cart/cartList.tiles";
 	}
 
